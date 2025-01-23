bf2cb3f161c3113fe281fc533ee0193911071912
D3mzy-108 <demil.ayinde@gmail.com>
2025-01-23 21:51:29 +0100

    Store, add to cart, and remove from cart features implemented

--- client_site/urls.py
+++ client_site/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home, store, add_to_cart, remove_from_cart, checkout
 
 urlpatterns = [
     path('', home, name='home'),
+    path('store/', store, name='store'),
+    path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:product_id>/',
+         remove_from_cart, name='remove_from_cart'),
+    path('checkout/', checkout, name='checkout'),
 ]
--- client_site/views.py
+++ client_site/views.py
@@ -1,6 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from manufacturer_site.classifications.models import Product, Packaging, ProductType
+from django.db.models import Q
 
 
 def home(request):
     context = {}
     return render(request, 'client_site/home.html', context)
+
+
+def store(request):
+    package = request.GET.get('pkg') or ''
+    product_type = request.GET.get('pty') or ''
+    query = request.GET.get('q') or ''
+    products = Product.objects.filter(
+        Q(product_color__icontains=query) &
+        Q(product_type__id__icontains=product_type) &
+        Q(package__id__icontains=package)
+    ).order_by('?')
+    context = {
+        'products': products,
+        'packaging': Packaging.objects.all(),
+        'product_types': ProductType.objects.all(),
+        'cart': request.session.get('cart', []),
+    }
+    return render(request, 'client_site/store.html', context)
+
+
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart', [])
+    cart.append(product_id)
+    request.session['cart'] = cart
+    return redirect(request.META.get('HTTP_REFERER', 'store'))
+
+
+def remove_from_cart(request, product_id):
+    cart = request.session.get('cart', [])
+    if len(cart) > 0:
+        cart.remove(product_id)
+    request.session['cart'] = cart
+    return redirect(request.META.get('HTTP_REFERER', 'checkout'))
+
+
+def checkout(request):
+    cart = request.session.get('cart', [])
+    products = Product.objects.filter(pk__in=cart)
+    context = {
+        'products': products,
+        'cart': cart,
+    }
+    return render(request, 'client_site/checkout.html', context)
