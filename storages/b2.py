2cc065cb78d419f32de0308d33468f9648f93ce9
D3mzy-108 <demil.ayinde@gmail.com>
2025-01-25 19:16:07 +0100

    implemented backblaze storage for media files

--- storages/b2.py
+++ storages/b2.py
@@ -1,29 +1,39 @@
 from django.core.files.storage import Storage
-from django.conf import settings
+from core import settings
 from b2sdk.v2 import InMemoryAccountInfo, B2Api
 import os
 
 
 class BackblazeB2Storage(Storage):
     def __init__(self):
-        info = InMemoryAccountInfo()
-        self.b2_api = B2Api(info)
+        self.account_info = InMemoryAccountInfo()
+        self.b2_api = B2Api(self.account_info)
+        print(settings.AWS_ACCESS_KEY_ID)
+        print(settings.AWS_SECRET_ACCESS_KEY)
         self.b2_api.authorize_account(
             "production", settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
-        self.bucket = self.b2_api.get_bucket_by_name(
-            settings.AWS_STORAGE_BUCKET_NAME)
+        self.bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+        self.bucket = self.b2_api.get_bucket_by_name(self.bucket_name)
 
     def _save(self, name, content):
         name = self._clean_name(name)
         content.open()
-        file_data = content.read()
-        self.bucket.upload_bytes(file_data, name)
-        content.close()
+        try:
+            file_data = content.read()
+            self.bucket.upload_bytes(file_data, name)
+        except Exception as e:
+            raise IOError(f"Error uploading file {name}: {e}")
+        finally:
+            content.close()
         return name
 
     def _open(self, name, mode='rb'):
-        # This can be implemented if you want to retrieve the file
-        pass
+        name = self._clean_name(name)
+        try:
+            file_info = self.bucket.download_file_by_name(name)
+            return file_info.download_as_bytes()
+        except Exception as e:
+            raise IOError(f"Error retrieving file {name}: {e}")
 
     def url(self, name):
         name = self._clean_name(name)
@@ -31,8 +41,13 @@ class BackblazeB2Storage(Storage):
 
     def exists(self, name):
         name = self._clean_name(name)
-        file_versions = self.bucket.list_file_versions(file_name=name)
-        return any(file_version.file_name == name for file_version in file_versions)
+        try:
+            for file_version in self.bucket.list_file_versions(file_name=name):
+                if file_version.file_name == name:
+                    return True
+            return False
+        except Exception:
+            return False
 
     def _clean_name(self, name):
         return os.path.normpath(name).replace('\\', '/')
