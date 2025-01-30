890a75e9dc4107149265200bedf88b039e1db6b1
D3mzy-108 <demil.ayinde@gmail.com>
2025-01-30 15:55:13 +0100

    added trending and profitability analysis to dashboard

--- manufacturer_site/dashboard/views.py
+++ manufacturer_site/dashboard/views.py
@@ -7,7 +7,7 @@ from manufacturer_site.inventory.models import RawMaterial, MaterialPurchaseLog
 from manufacturer_site.production.models import Production
 from manufacturer_site.classifications.models import Product
 from manufacturer_site.cashier.models import Sale
-from django.db.models import Sum, F, FloatField, ExpressionWrapper
+from django.db.models import Sum, F, FloatField, ExpressionWrapper, Count
 
 
 @login_required
@@ -33,6 +33,10 @@ def dashboard(request):
     material_purchase_logs = MaterialPurchaseLog.objects.filter(
         date__range=[start_date, today])
     sales = Sale.objects.filter(order__date__range=[start_date, today])
+
+    product_performance = _product_performance(
+        sales, start_date, today, limit=5)
+
     context = {
         'summary': _get_summary(materials=raw_materials, productions=productions, products=products, sales=sales, material_purchases=material_purchase_logs),
         'date_query': date_query,
@@ -44,6 +48,10 @@ def dashboard(request):
         'low_stock_raw_materials': raw_materials.filter(quantity_in_stock__lt=50).order_by('-id'),
         'live_productions': productions.filter(is_completed=False),
         'production_cost_over_time': _get_production_cost_over_time(productions),
+        'top_selling_products': product_performance['top_grossing'],
+        'trending_products': product_performance['trending'],
+        'most_popular': product_performance['most_popular'],
+        'most_profitable': product_performance['most_profitable'],
     }
     return render(request, 'manufacturer_site/dashboard/dashboard.html', context)
 
@@ -102,3 +110,38 @@ def _get_production_cost_over_time(productions) -> str:
             ctx['x'].append(p_date)
             ctx['y'].append(production.batch.ttl_cost)
     return json.dumps(ctx)
+
+
+def _product_performance(sales, start_date, end_date, limit=10):
+    grossing = {
+        'x': [],
+        'y': [],
+    }
+    trending = {
+        'x': [],
+        'y': [],
+    }
+    if sales.exists():
+        # GET TOP PRODUCTS WITH THE MOST REVENUE
+        most_revenue = Product.objects.filter(product_sales__order__date__range=[start_date, end_date]).annotate(
+            total_revenue=Sum('product_sales__total_cost'),
+            profit=Sum(ExpressionWrapper((F('product_sales__total_cost') - (F('product_sales__quantity_sold') * F('product_sales__product__cost_price'))), output_field=FloatField()))).order_by('-profit')[:limit]
+        for product in most_revenue:
+            grossing['x'].append(
+                f'{product.product_color} {product.product_type.name} ({product.package.volume} lts)')
+            grossing['y'].append(product.total_revenue or 0)
+
+        # GET TOP PRODUCTS WITH THE MOST ORDERS
+        most_purchased = Product.objects.filter(product_sales__order__date__range=[start_date, end_date]).annotate(
+            order_count=Count('product_sales__order')).order_by('-order_count')[:limit]
+        for product in most_purchased:
+            trending['x'].append(
+                f'{product.product_color} {product.product_type.name} ({product.package.volume} lts)')
+            trending['y'].append(product.order_count or 0)
+
+    return {
+        'top_grossing': json.dumps(grossing),
+        'trending': json.dumps(trending),
+        'most_popular': most_purchased.first() if most_purchased else None,
+        'most_profitable': most_revenue.first() if most_revenue else None,
+    }
